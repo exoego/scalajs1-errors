d4439d80b61fc882130c2148534a11e743dac309
exoego <ytatsuno.jp@gmail.com>
2020-06-02 11:45:14 +0900

    wip

--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -3,7 +3,7 @@ import io.lemonlabs.uri.Url
 object Foo {
   def main(args: Array[String]): Unit = {
     val params = Map.empty[String,String]
-    val queryParams = Url.parse("/").addParams(params.map(v => v._1 -> v._2))
-    println(queryParams)
+    val withParams = Url.parse("/").addParams(params)
+    println(withParams)
   }
 }
